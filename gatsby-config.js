387c6c233e125fe3f8abe917dff8a456ab5bb3d9
Laza Nantenaina <laza@hairun-technology.com>
2021-08-30 21:27:15 +0300

    💄 Final style configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'my-portfolio'
   },
-  plugins: []
+  plugins: [
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        resetCSS: true,
+        isUsingColorMode: true
+      }
+    }
+  ]
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <>Test</>;
+  return <></>;
 }
 
 export default Home;
