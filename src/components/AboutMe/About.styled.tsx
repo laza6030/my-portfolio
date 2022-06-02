9edb3834bb1c2b000078c2184a6386ea5cefab3e
Laza Nantenaina <laza@hairun-technology.com>
2022-06-02 22:55:40 +0300

    Add self description

--- src/components/AboutMe/About.styled.tsx
+++ src/components/AboutMe/About.styled.tsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
 export const MainWrapper = styled.div`
+  padding-left: 100px;
+`;
+
+export const Wrapper = styled.div`
   display: flex;
   justify-content: space-around;
   margin-top: 80px;
+`;
+
+export const Description = styled.div`
+  width: 50%;
+  color: ${({ theme }) => theme.text.white};
+  font-family: Nunito;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 export const TitleWrapper = styled.div`
