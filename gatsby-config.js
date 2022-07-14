75441fc217c608f2fb379b1620edaedea5c97814
Laza Nantenaina <laza@hairun-technology.com>
2022-07-14 19:00:20 +0300

    Try random stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
-    // `gatsby-plugin-sharp`,
     // `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-google-fonts`,
