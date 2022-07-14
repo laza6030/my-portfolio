4ce60211c1d9fc6bc81eadb62d62248ead4a2ceb
Laza Nantenaina <laza@hairun-technology.com>
2022-07-14 19:06:07 +0300

    Fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: ``, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
     // `gatsby-plugin-react-helmet`,
