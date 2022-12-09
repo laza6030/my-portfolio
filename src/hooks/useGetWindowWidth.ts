5c46ff691f5a39a17c1bd9a6076966cd06cb3828
Laza Nantenaina <laza@hairun-technology.com>
2022-12-09 11:37:17 +0300

    💚 Fix build

--- src/hooks/useGetWindowWidth.ts
+++ src/hooks/useGetWindowWidth.ts
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import { MOBILE_DEVICE_WIDTH } from "../constants";
 
+const SCREEN_DEFAULT_WIDTH = 1024; // This is an arbitrary choice
+
 export const useGetWindowWidth = () => {
-  const [width, setWidth] = useState(screen.width);
+  const [width, setWidth] = useState(SCREEN_DEFAULT_WIDTH);
 
   useEffect(() => {
-    window.addEventListener("resize", () => setWidth(screen.width));
-    return window.removeEventListener("resize", () => setWidth(screen.width));
+    if (screen) {
+      window.addEventListener("resize", () => setWidth(screen.width));
+      return window.removeEventListener("resize", () => setWidth(screen.width));
+    }
   }, []);
 
   const isMobile = width <= MOBILE_DEVICE_WIDTH;
