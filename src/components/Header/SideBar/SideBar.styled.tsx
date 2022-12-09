8da0339a38983ab3fabaa84aaf1d984b58e58e65
Laza Nantenaina <laza@hairun-technology.com>
2022-12-09 18:18:39 +0300

    💫 Add animation to sidebar

--- src/components/Header/SideBar/SideBar.styled.tsx
+++ src/components/Header/SideBar/SideBar.styled.tsx
@@ -1,6 +1,18 @@
 import { Link } from "gatsby";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { Close } from "@styled-icons/ionicons-outline/Close";
+
+const animation = keyframes`
+  from {
+    opacity: 0;
+    right: -100px;
+  }
+
+  to {
+    opacity: 1;
+     right: 0px;
+  }
+`;
 
 export const MainWrapper = styled.div`
   position: relative;
@@ -12,6 +24,8 @@ export const MainWrapper = styled.div`
   height: 200px;
   justify-content: space-between;
   padding-bottom: 50px;
+
+  animation: ${animation} 0.5s ease-out;
 `;
 
 export const CloseMenuIcon = styled(Close)`
