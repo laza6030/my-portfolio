8e8fcd763e6a83a7a1f9f44433a0751d8fba23af
Laza Nantenaina <laza@hairun-technology.com>
2023-11-01 18:12:06 +0300

    Add particle background

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,7 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useCallback } from "react";
+
+import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
 
 import Header from "../Header";
 import Footer from "../Footer";
@@ -10,8 +13,59 @@ interface IProps {
 }
 
 const Layout = ({ children }: IProps) => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
   return (
     <>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          fpsLimit: 120,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 0.5,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 40,
+            },
+            opacity: {
+              value: 0.1,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Header />
       <Wrapper>{children}</Wrapper>
       <Footer />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { ThemeProvider } from "styled-components";
-
 import { Helmet } from "react-helmet";
 
 // import Career from "../components/Career";
